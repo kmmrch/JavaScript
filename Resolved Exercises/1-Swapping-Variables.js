41e09e7b9c7ab014cdbff93f00ef3cbd6a3094d2
Mariana R. Kemmerich <123078429+marianakemmerich@users.noreply.github.com>
2023-04-01 19:54:32 -0300

    Create 1-Swapping-Variables.js

---
+++ Resolved Exercises/1-Swapping-Variables.js
@@ -0,0 +1,12 @@
+let a = 'red';
+let b = 'blue';
+
+console.log(a); // red
+console.log(b); // blue
+
+let c = a; // this variable will be used to swap a and b's values
+a = b; // blue
+b = c; // red
+
+console.log(a); // blue
+console.log(b); // red
