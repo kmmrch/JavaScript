f964a277b54577afb2423f9977b4afeabe744066
Mariana R. Kemmerich <123078429+marianakemmerich@users.noreply.github.com>
2023-04-02 18:41:35 -0300

    Create 2-Max-of-Two-Numbers.js

---
+++ Resolved Exercises/2-Max-of-Two-Numbers.js
@@ -0,0 +1,10 @@
+// Write a function that takes two numbers and returns the maximum of the two:
+
+let number = max(1, 2);
+console.log(number);
+function max(number1, number2){
+    if(number1 > number2) return a;
+    return b;
+
+// It can also be written like this: return (a > b) ? a : b; which is the equivalent to the 2 lines above
+}
